1b18b89c81d5531039c3ae0b5c1b9b992229ac78
Christentimmy <timmychris09@gmail.com>
2024-11-18 21:09:31 +0100

    chat menu screen done

--- lib/pages/chat_menu_screen.dart
+++ lib/pages/chat_menu_screen.dart
@@ -3,7 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ChatMenuScreen extends StatelessWidget {
-  const ChatMenuScreen({super.key});
+  ChatMenuScreen({super.key});
+
+  List _issues = [
+    'Accepted trip by accident',
+    "Problem with pickup route",
+    "Made a wrong turn",
+    "Pickup isn’t worth it",
+    "Not safe to pick up",
+    'Vehicle issue'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +37,66 @@ class ChatMenuScreen extends StatelessWidget {
           ),
         ),
         actions: [
-          Icon(Icons.call, color: AppColors.primaryColor,),
+          Icon(
+            Icons.call,
+            color: AppColors.primaryColor,
+          ),
           const SizedBox(width: 10),
           // Icon(Icons.more_vert_sharp),
           // SizedBox(width: 15),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+        child: Column(
+          children: [
+            SizedBox(height: Get.height * 0.05),
+            Text(
+              "Something wrong? Choose an issue: ",
+              style: TextStyle(
+                color: AppColors.primaryColor,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: Get.height * 0.09),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _issues.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Icon(
+                      Icons.cancel_sharp,
+                      color: AppColors.primaryColor,
+                    ),
+                    title: Text(
+                      _issues[index],
+                      style: const TextStyle(
+                        fontSize: 13,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Divider(),
+            ListTile(
+              leading: Icon(
+                Icons.more_vert,
+                color: AppColors.primaryColor,
+              ),
+              title: const Text(
+                "More Issues",
+                style: TextStyle(
+                  fontSize: 13,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

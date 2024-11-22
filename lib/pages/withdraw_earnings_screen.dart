9a2230447c9d61a4bf28b3a63bc74ef20cee3c30
Christentimmy <timmychris09@gmail.com>
2024-11-22 21:39:14 +0100

    withdraw earnings screen

---
+++ lib/pages/withdraw_earnings_screen.dart
@@ -0,0 +1,121 @@
+import 'package:eco/Resources/color_resources.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class WithdrawEarningsScreen extends StatelessWidget {
+  const WithdrawEarningsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        title: const Text(
+          "Withdraw Earnings",
+          style: TextStyle(
+            fontSize: 15,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 15,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: Get.height * 0.02),
+            Text(
+              "Choose bank Account",
+              style: TextStyle(
+                color: AppColors.primaryColor,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              height: 50,
+              width: Get.width,
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/access.png"),
+                  const SizedBox(width: 10),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Christen Timmy",
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        "************45",
+                        style: TextStyle(
+                          fontSize: 9,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              height: 50,
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                border: Border.all(width: 2, color: AppColors.primaryColor),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.payment,
+                    color: AppColors.primaryColor,
+                  ),
+                  const SizedBox(width: 15),
+                  const Text("Add New Bank")
+                ],
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.camera_alt_rounded, color: Colors.white),
+                SizedBox(width: 7),
+                Text(
+                  "Scan Card",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

1ac65a139ade24e182a98af74238deaa21e6ecf4
Christentimmy <timmychris09@gmail.com>
2024-12-09 00:47:41 +0100

    Ride details screen

--- lib/pages/home/request_history_screen.dart
+++ lib/pages/home/request_history_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eco/pages/home/ride_details_screen.dart';
 import 'package:eco/resources/color_resources.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -21,109 +22,117 @@ class RequestHistoryScreen extends StatelessWidget {
           children: [
             _buildDayWeekNav(),
             const SizedBox(height: 30),
-            Container(
-              width: Get.width,
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              constraints: BoxConstraints(
-                minHeight: 55,
+            _buildRequestFilterCards(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRequestFilterCards() {
+    return Container(
+      width: Get.width,
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      constraints: BoxConstraints(
+        minHeight: 55,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          width: 2,
+          color: Colors.white,
+        ),
+      ),
+      child: Column(
+        children: [
+          _buildFilterHeader(),
+          Obx(
+            () {
+              if (_isExpand.value) {
+                return Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    _buildFirstTile(
+                      amount: "\$500.00",
+                      time: "08:00 - 10:00",
+                    ),
+                    _buildSecondTile(
+                      amount: "\$30.00",
+                      time: "09:00 - 12:00",
+                    ),
+                    _buildFirstTile(
+                      amount: "\$900.00",
+                      time: "10:00 - 2:00",
+                    ),
+                    _buildSecondTile(
+                      amount: "\$1000.00",
+                      time: "02:00 - 6:00",
+                    ),
+                    _buildFirstTile(
+                      amount: "\$500.00",
+                      time: "08:00 - 10:00",
+                    ),
+                    _buildSecondTile(
+                      amount: "\$30.00",
+                      time: "09:00 - 12:00",
+                    ),
+                  ],
+                );
+              }
+              return SizedBox();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  Row _buildFilterHeader() {
+    return Row(
+      children: [
+        Column(
+          children: [
+            Text(
+              "26.09",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
               ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(
-                  width: 2,
-                  color: Colors.white,
-                ),
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            "26.09",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            "Done: 1",
-                            style: TextStyle(
-                              color: Colors.white.withOpacity(0.5),
-                              fontSize: 13,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                      Text(
-                        "\$50.00",
-                        style: TextStyle(
-                          color: AppColors.primaryColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Obx(
-                        () => IconButton(
-                          onPressed: () => _isExpand.value = !_isExpand.value,
-                          icon: _isExpand.value
-                              ? Icon(
-                                  Icons.keyboard_arrow_up_rounded,
-                                  color: Colors.white,
-                                )
-                              : Icon(
-                                  Icons.keyboard_arrow_down_outlined,
-                                  color: Colors.white,
-                                ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Obx(
-                    () {
-                      if (_isExpand.value) {
-                        return Column(
-                          children: [
-                            const SizedBox(height: 10),
-                            _buildFirstTile(
-                              amount: "\$500.00",
-                              time: "08:00 - 10:00",
-                            ),
-                            _buildSecondTile(
-                              amount: "\$30.00",
-                              time: "09:00 - 12:00",
-                            ),
-                            _buildFirstTile(
-                              amount: "\$900.00",
-                              time: "10:00 - 2:00",
-                            ),
-                            _buildSecondTile(
-                              amount: "\$1000.00",
-                              time: "02:00 - 6:00",
-                            ),
-                            _buildFirstTile(
-                              amount: "\$500.00",
-                              time: "08:00 - 10:00",
-                            ),
-                            _buildSecondTile(
-                              amount: "\$30.00",
-                              time: "09:00 - 12:00",
-                            ),
-                          ],
-                        );
-                      }
-                      return SizedBox();
-                    },
-                  )
-                ],
+            ),
+            Text(
+              "Done: 1",
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.5),
+                fontSize: 13,
               ),
             ),
           ],
         ),
-      ),
+        const Spacer(),
+        Text(
+          "\$50.00",
+          style: TextStyle(
+            color: AppColors.primaryColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+        ),
+        const SizedBox(width: 10),
+        Obx(
+          () => IconButton(
+            onPressed: () => _isExpand.value = !_isExpand.value,
+            icon: _isExpand.value
+                ? Icon(
+                    Icons.keyboard_arrow_up_rounded,
+                    color: Colors.white,
+                  )
+                : Icon(
+                    Icons.keyboard_arrow_down_outlined,
+                    color: Colors.white,
+                  ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -156,27 +165,32 @@ class RequestHistoryScreen extends StatelessWidget {
     );
   }
 
-  Container _buildFirstTile({
+  Widget _buildFirstTile({
     required String time,
     required String amount,
   }) {
-    return Container(
-      height: 40,
-      width: Get.width,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      padding: const EdgeInsets.symmetric(
-        horizontal: 10,
-        vertical: 6,
-      ),
-      child: Row(
-        children: [
-          Text(time),
-          Spacer(),
-          Text(amount),
-        ],
+    return GestureDetector(
+      onTap: () {
+        Get.to(() => RideDetailsScreen());
+      },
+      child: Container(
+        height: 40,
+        width: Get.width,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 6,
+        ),
+        child: Row(
+          children: [
+            Text(time),
+            Spacer(),
+            Text(amount),
+          ],
+        ),
       ),
     );
   }
@@ -265,4 +279,5 @@ class RequestHistoryScreen extends StatelessWidget {
       ),
     );
   }
+
 }
---
+++ lib/pages/home/ride_details_screen.dart
@@ -0,0 +1,223 @@
+import 'package:eco/resources/color_resources.dart';
+import 'package:flutter/material.dart';
+
+class RideDetailsScreen extends StatelessWidget {
+  const RideDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 20,
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 30),
+            _buildRideHeader(),
+            const SizedBox(height: 20),
+            TableWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRideHeader() {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              "Balance:  ",
+              style: TextStyle(
+                color: AppColors.primaryColor,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "\$60.00",
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.5),
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            Text(
+              "Refund:  ",
+              style: TextStyle(
+                color: AppColors.primaryColor,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "\$60.00",
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.5),
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: [
+            Text(
+              "Bonuses:  ",
+              style: TextStyle(
+                color: AppColors.primaryColor,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "\$60.00",
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.5),
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.black,
+      centerTitle: true,
+      leading: IconButton(
+        onPressed: () {},
+        icon: Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
+      ),
+      title: Text(
+        "Ride Details",
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class TableWidget extends StatelessWidget {
+  const TableWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      border: TableBorder.all(
+        color: Colors.grey.withOpacity(0.3),
+        width: 2,
+      ),
+      columnWidths: const {
+        0: FractionColumnWidth(0.4),
+        1: FractionColumnWidth(0.6),
+      },
+      children: [
+        // Header row
+        TableRow(
+          decoration: BoxDecoration(color: AppColors.primaryColor),
+          children: const [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Order Type',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'The Payment for completed Request',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ),
+          ],
+        ),
+
+        // Row 1
+        TableRow(
+          decoration: BoxDecoration(color: Color(0xffEDEDED)),
+          children: const [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Express',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Min 2 Soles with 10% Commission to company',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ],
+        ),
+
+        // Row 2
+        TableRow(
+          decoration: BoxDecoration(color: Color(0xffEDEDED)),
+          children: const [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Flete',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Min 2 Soles plus 1 sole extra charge with 10% Commission to company',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
